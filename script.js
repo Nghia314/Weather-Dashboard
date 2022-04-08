f4699ac71918d830580bd96537195d3c944ed5b8
Nghia Le <davidle314@hotmail.com>
2022-04-08 10:48:39 -0700

    declaring varible for Js

---
+++ script.js
@@ -0,0 +1,12 @@
+var cities = [];
+//varible for search box
+var citySearch = document.getElementById("search-city-box");
+var cityInput = document.getElementById("city");
+var searchbutton = document.getElementById("search-button");
+var historybutton = document.getElementById("clear-history");
+var weatherContainer = document.getElementById("current-weather-container");
+var selectedCity = document.getElementById("selected");
+// varible for  5 day weaather
+var fivedayweather = document.getElementById("fivedayweather");
+var weather = document.getElementById("weather");
+var fiveday = document.getElementById("fiveday");
